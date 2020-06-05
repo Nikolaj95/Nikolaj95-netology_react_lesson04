c3aea18b4916e45c7ea72e3ef4e76112683e47be
Nikolay Konyukov <konyukov142@gmail.com>
2020-06-05 21:34:51 +0700

    задание 2

---
+++ steps/src/components/WalksRecords.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import AddRecord from './AddRecord';
+import RecordsListing from './RecordsListing';
+
+export default function WallksRecords() {
+    const [walks, setWalks] = useState([]);
+    const [form, setForm] = useState({date: '', distance: ''});
+
+    return (<>
+        <AddRecord form={form} setForm={setForm} setWalks={setWalks}/>
+        <RecordsListing walks={walks} setForm={setForm} setWalks={setWalks}/>
+    </>)
+}
