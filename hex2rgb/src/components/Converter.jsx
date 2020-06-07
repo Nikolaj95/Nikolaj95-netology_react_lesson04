f9a979c1416d85fe2d2f590f331ac1d22a3b3b83
Nikolay Konyukov <konyukov142@gmail.com>
2020-06-07 14:56:42 +0700

    исправил задание 1

--- hex2rgb/src/components/Converter.jsx
+++ hex2rgb/src/components/Converter.jsx
@@ -7,18 +7,18 @@ export default function Converter() {
     const [hex, setHex] = useState("#");
     const [rgb, setRgb] = useState("");
     const [err, setErr] = useState(false);
+    const [hide, setClassHide] = useState(true);
 
     const handleHex = e => {
         const target = e.target;
-        const result = document.querySelector('#result');
 
         if (target.value.length <= 7) {
-            result.classList.add('hide');
             if (!target.value) {
                 setHex("#")
             } else {
                 setHex(target.value);
             }
+            setClassHide(true);
             setRgb("");
             setErr(false);
         }
@@ -26,19 +26,18 @@ export default function Converter() {
         if (target.value.length === 7) {
             const res = hexToRgb(target.value);
             setRgb(res || errorBG);
-            result.classList.remove('hide');
-
+            setClassHide(false);
             if (!res) {
                 setErr(true);
             }
-
         }
     };
 
     return (
         <div className="converter" style={{background: rgb}}>
             <input type="text" defaultValue={hex} onChange={handleHex} placeholder="#123456"/>
-            <input className="result hide" type="text" id="result" defaultValue={err ? "Ошибка!" : rgb}
+            <input className={hide ? "result hide" : "result"} type="text" id="result"
+                   defaultValue={err ? "Ошибка!" : rgb}
             />
         </div>
     );
